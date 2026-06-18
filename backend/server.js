bcf6ed1c05fe7ba25bb0bd68c83a91e7e02c72fe
reema alsubaie <reema11alsub@gmail.com>
2026-06-18 03:36:05 +0300

    fix express wildcard error

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.options('*', cors());
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth',         require('./routes/auth'));
